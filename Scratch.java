4f33bca414ae01ffac435b323fa298c70d41cdc7
clion94 <39510594+clion94@users.noreply.github.com>
2022-06-29 21:28:47 -0500

    Update Scratch.java

--- Scratch.java
+++ Scratch.java
@@ -1,6 +1,6 @@
 /*
 Given a sorted array, find the smallest positive integer that is not the sum of a subset of the array.
-For example, for the input [1, 2, 3, 10], you should return 7.
+For example, for the input [1, 2, 3, 10], you should return 7. Do this in O(N) time.
 
 Input:  arr[] = {1, 10, 3, 11, 6, 15};
 Output: 2
